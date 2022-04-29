21b61c328337a013348ed1100dd21cbcb62bbaaa
hogueq <quinnhogue24@gmail.com>
2022-04-29 12:33:33 -0400

    Changed startup script, I added the commands to the end of the home/.bashrc file and removed the sudo killall pigpio command completely

--- RoboticArmStartupScript.sh
+++ RoboticArmStartupScript.sh
@@ -1,7 +1,2 @@
-export DISPLAY=:0.0
-sudo killall pigpiod
-sudo systemctl start pigpiod
-export PIGPIO_ADDR=localhost
-export PIGPIO_PORT=8888
 cd ~/Desktop/Robot_Arm_Controller_GUI
 python3 main.py
